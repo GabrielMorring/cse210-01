ad818523b5c7388cd0a21fb125be5c85e8fd5a70
Gabriel Morring <gmorring7@gmail.com>
2022-01-15 13:33:56 -0800

    basic game finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,140 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+static Boolean checkWin(List<int> Tiles)
+{
+    if (Tiles.Contains(1) && Tiles.Contains(2) && Tiles.Contains(3))
+    {
+        return true;
+    }
+    else if (Tiles.Contains(4) && Tiles.Contains(5) && Tiles.Contains(6))
+    {
+        return true;
+    }
+    else if (Tiles.Contains(7) && Tiles.Contains(8) && Tiles.Contains(9))
+    {
+        return true;
+    }
+    else if (Tiles.Contains(1) && Tiles.Contains(4) && Tiles.Contains(7))
+    {
+        return true;
+    }
+    else if (Tiles.Contains(2) && Tiles.Contains(5) && Tiles.Contains(8))
+    {
+        return true;
+    }
+    else if (Tiles.Contains(3) && Tiles.Contains(6) && Tiles.Contains(9))
+    {
+        return true;
+    }
+    else if (Tiles.Contains(1) && Tiles.Contains(5) && Tiles.Contains(9))
+    {
+        return true;
+    }
+    else if (Tiles.Contains(3) && Tiles.Contains(5) && Tiles.Contains(7))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+static string changePlayer(string player)
+{
+    if (player == "X")
+    {
+        return "O";
+    }
+    else if (player == "O")
+    {
+        return "X";
+    }
+    else 
+    {
+        return "";
+    }
+}
+
+static void playGame()
+{
+    List<string> board = new List<string>() {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    List<int> xTiles = new List<int>();
+    List<int> oTiles = new List<int>();
+    string currentPlayer = "X";
+    string winner = "";
+    Boolean prevChechWin = false;
+    
+    Console.WriteLine();
+
+    while (prevChechWin == false)
+    {        
+        Console.WriteLine($"{board[0]} | {board[1]} | {board[2]}");
+        Console.WriteLine("_________");
+        Console.WriteLine($"{board[3]} | {board[4]} | {board[5]}");
+        Console.WriteLine("_________");
+        Console.WriteLine($"{board[6]} | {board[7]} | {board[8]}");
+        
+
+        Console.WriteLine();
+        Console.WriteLine($"Player {currentPlayer}");
+        Console.WriteLine("Enter the number of the place you want to put a tile.");
+        
+        int newTile = int.Parse(Console.ReadLine());
+
+        int tilePosition = newTile - 1;
+
+        if (currentPlayer == "X")
+        {
+            xTiles.Add(newTile);
+            if (checkWin(xTiles))
+            {
+                winner = "X";
+            }
+            board[tilePosition] = "X";
+            prevChechWin = checkWin(xTiles);
+        }
+        else if (currentPlayer == "O")
+        {
+            oTiles.Add(newTile);
+            if (checkWin(oTiles))
+            {
+                winner = "O";
+            }
+            board[tilePosition] = "O";
+            prevChechWin = checkWin(oTiles);
+        }
+
+        
+
+
+        currentPlayer = changePlayer(currentPlayer);  
+    
+    }
+    Console.WriteLine($"{board[0]} | {board[1]} | {board[2]}");
+    Console.WriteLine("_________");
+    Console.WriteLine($"{board[3]} | {board[4]} | {board[5]}");
+    Console.WriteLine("_________");
+    Console.WriteLine($"{board[6]} | {board[7]} | {board[8]}");
+    
+    Console.WriteLine();
+    Console.WriteLine($"Congradulations, You win player {winner}!");
+}
+
+static void main()
+{
+    string action = "a";
+    Console.WriteLine("Welcome to Tic-Tac-Toe");
+    while (action == "a")
+    {
+        playGame();
+        Console.WriteLine("Would you like to:");
+        Console.WriteLine("a) play again");
+        Console.WriteLine("b) quit");
+        action = Console.ReadLine();
+    }
+
+}
+
+main();
